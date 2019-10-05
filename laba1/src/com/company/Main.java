ec78fe1e8f68f4bcd1f24d84cc3434507bfad223
lizardpif <lizka.pif@mail.ru>
2019-10-05 12:22:17 +0300

    resolve conflicts

--- laba1/src/com/company/Main.java
+++ laba1/src/com/company/Main.java
@@ -1,12 +1,8 @@
 package com.company;
 
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
@@ -14,50 +10,67 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+
         System.out.println("укажите имя файла конфигурации");
 
-        String name = in.nextLine();
-        //System.out.println("-> condition.json");
+        String name = "Condition.json";
+        //String name = in.nextLine();
+        System.out.println("-> " + name);
+
         Condition con = new Condition(name);
 
         Map<String, String> leftBracket = new HashMap<>();
         Map<String, String> rightBracket = new HashMap<>();
+
         con.readCondition(leftBracket, rightBracket);
 
         System.out.println("укажите имя файла для проверки скобок");
-        name = in.nextLine();// "brackets.txt";
-        //System.out.println("-> brackets.txt");
-        //name = "brackets.txt";
+        //name = in.nextLine();// "brackets.txt";
 
-        try(FileReader f = new FileReader(name)){
+        name = "brackets.txt";
+        System.out.println("-> " + name);
+
+        //create vector for errors
+        Stack<Integer> errors = new Stack<>();
+        int counter = 1;
+
+        try (FileReader f = new FileReader(name)) {
             Stack<String> brackets = new Stack<>();
-            int c; //а если скобка составная?????
-            while ( (c = f.read() ) != -1){
-                String s = Character.toString((char)c);
-                if (leftBracket.get(s) != null || rightBracket.get(s) != null ){
+            int c; //а если скобка составная?????а?
+            while ((c = f.read()) != -1) {
+                String s = Character.toString((char) c);
+                if (leftBracket.get(s) != null || rightBracket.get(s) != null) {
                     //стек не пуст, тогда
 
-                    if (!brackets.isEmpty()){
-                        String str = brackets.peek().toString(); //берем верхнее со стека
+                    if (!brackets.isEmpty()) {
+                        String str = brackets.peek(); //берем верхнее со стека
                         String str2 = leftBracket.get(str); //берем по ключу
-                        if (str2!=null && str2.equals(s)) //если строки равны
+                        if (str2 != null && str2.equals(s)) //если строки равны
+                        {
                             brackets.pop();
-                        else
+                            errors.pop();
+                        } else {
                             brackets.push(s);
-                    }
-                    else
+                            errors.push(counter);
+                        }
+                    } else {
                         brackets.push(s);
+                        errors.push(counter);
+                    }
                 }
+                counter++;
 
             }
             if (brackets.isEmpty())
                 System.out.println("скобки в файле расставлены правильно");
-            else
+            else {
                 System.out.println("скобки в файле расставлены НЕ правильно");
+                System.out.println(brackets);
+                System.out.println(errors);
+            }
 
-        }
-        catch(Exception e){
-            System.out.println("error");
+        } catch (IOException e) {
+            System.err.println("error");
         }
 
     }
